b467f7615331de7aab2c0c6e6a00c258201af74d
David Fisher <fisherds@rose-hulman.edu>
2022-03-08 09:06:14 -0500

    One slider working

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.value = 0.8
+        updateView()
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        updateView()
+    }
+    
+    func updateView() {
+        print("Red slider value is \(redSlider.value)")
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        
+    }
 }
 
